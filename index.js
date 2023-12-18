b53a9985ec75792230f83ae5506ad498c72f5898
bakslash <marcusmuasa@gmail.com>
2023-12-18 14:03:33 +0300

    bugfix

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(bodyParser.json());
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5432;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
