e57957cbc6ca7aff33591c0a8406ca5f6270a768
bakslash <marcusmuasa@gmail.com>
2023-12-19 18:02:01 +0300

    bugs

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -106,11 +106,13 @@ exports.getUsers = async (req, res) => {
       return res.status(401).json({ success: false, message: 'Invalid email or password' });
     }
     //bug for later admin login bypass
-   if(user.role!=='admin'|| user.password!==password){
+   
 
     // Compare the entered password with the hashed password in the database
-    const passwordMatch = await bcrypt.compare(password, user.password);
-}
+   // const passwordMatch = await bcrypt.compare(password, user.password);
+//admin by pass
+   const passwordMatch = (password == user.password)
+   console.log('check',passwordMatch);
 
     if (!passwordMatch) {
       return res.status(401).json({ success: false, message: 'Invalid email or password' });
